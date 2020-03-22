910b0c202142c91936f4e5377a9dfe50812b42b3
Abdullah Ahmed <abdullah@softcomnetworks.com>
2020-03-22 10:27:36 +0000

    Fixed Styling

--- crypted/encrypt/views.py
+++ crypted/encrypt/views.py
@@ -7,6 +7,16 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 
 
 def encryptData(pub_key, data):
+    """
+        Function that encrypts data using key and returns HTTP download response
+
+        Parameters:
+        pub_key : request.FILE
+        data : string
+
+        return:
+        HttpResponse
+    """
     data = data.encode("utf-8")
     filename = "encrypted.bin"
     # content = []
@@ -19,33 +29,27 @@ def encryptData(pub_key, data):
     rsa_cipher = PKCS1_OAEP.new(public_key)
     encrypted_session_key = rsa_cipher.encrypt(session_key)
 
-
     AES_cipher = AES.new(session_key, AES.MODE_EAX)
     encrypted_data, tag = AES_cipher.encrypt_and_digest(data)
 
-    content = b""
+    encrypted_payload = []
+    [encrypted_payload.append(x) for x in (encrypted_session_key, AES_cipher.nonce, tag, encrypted_data)]
 
-    l = []
-    [l.append(x) for x in (encrypted_session_key, AES_cipher.nonce, tag, encrypted_data)]
-
-    content.join(l)
-
-    response =  HttpResponse(l, content_type='text/plain')
+    response = HttpResponse(encrypted_payload, content_type='text/plain')
     response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
-
-    
-
-    
-
     return response
 
+
 def index(request):
+    """
+        View to show form to upload key and type in data
+        User gets an encrypted .bin file when successful
+    """
     if request.method == "POST":
         form = EncryptForm(request.POST, request.FILES)
         if form.is_valid():
             return encryptData(request.FILES['public_key'], form.cleaned_data['data'])
     else:
         form = EncryptForm()
-    
-    context = {'form':form}
+    context = {'form' : form}
     return render(request, 'encrypt/index.html', context)
--- crypted/genKeyPair/views.py
+++ crypted/genKeyPair/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES, PKCS1_OAEP
 
 
 def index(request):
